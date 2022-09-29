e40b86b784af19682be9a32107bf9aef5bda97f7
WannaratA <wnr.akvp@gmail.com>
2022-09-29 09:16:38 +0700

    AuthController: Unit Test

--- src/auth/test/auth.controller.spec.ts
+++ src/auth/test/auth.controller.spec.ts
@@ -33,4 +33,46 @@ describe('AuthCotroller', () => {
       });
     });
   });
+  describe('signinLocal', () => {
+    describe('when signinLocal get called', () => {
+      let token: Tokens;
+      beforeEach(async () => {
+        token = await controller.signinLocal(authDtoStub());
+      });
+      test('then it should call AuthService', () => {
+        expect(service.signinLocal).toBeCalledWith(authDtoStub());
+      });
+      test('then it should return tokens', () => {
+        expect(token).toEqual(tokenStub());
+      });
+    });
+  });
+  describe('logout', () => {
+    describe('when logout get called', () => {
+      let user: object;
+      beforeEach(async () => {
+        user = await controller.logout('test');
+      });
+      test('then it should call AuthService', () => {
+        expect(service.logout).toBeCalledWith('test');
+      });
+      test('then it should return empty object', () => {
+        expect(user).toEqual({});
+      });
+    });
+  });
+  describe('refreshToken', () => {
+    describe('when refreshToken get called', () => {
+      let token: Tokens;
+      beforeEach(async () => {
+        token = await controller.refreshToken('test', 'refreshTokenStub');
+      });
+      test('then it should call AuthService', () => {
+        expect(service.refreshToken).toBeCalledWith('test', 'refreshTokenStub');
+      });
+      test('then it should return tokens', () => {
+        expect(token).toEqual(tokenStub());
+      });
+    });
+  });
 });
